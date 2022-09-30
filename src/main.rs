a758dbfd4ad6ab8374f1fc524f9848e5a4b510db
Windore <windore.wi@protonmail.com>
2022-09-30 23:24:42 +0300

    Add main program error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use vihr::TimeBook;
+use vihr::*;
 
 use std::{env, fs};
+use std::path::Path;
 
 fn main() {
     let filename = env::var("VIHR_SAVE_FILE");
@@ -11,25 +12,59 @@ fn main() {
     }
 
     let filename = filename.unwrap();
+    let book;
 
-    let json = fs::read_to_string(&filename);
+    // Serde needs a string reference. It needs to live as long as `book`.
+    let json_str;
+
+    if Path::new(&filename).exists() {
+        let json = fs::read_to_string(&filename);
+
+        if let Err(e) = json {
+            eprintln!("Could not read save file '{}'.", filename);
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+
+        json_str = json.unwrap();
+
+        book = serde_json::from_str(&json_str);
+    } else {
+        println!("Save file doesn't exist. It will be created.");
+        book = Ok(TimeBook::default());
+    }
+
+    if let Err(e) = book {
+        eprintln!("Could not parse json from file '{}'.", filename);
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+
+    let mut book: TimeBook = book.unwrap();
+
+    if let Err(e) = handle_commands(&mut book) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+
+    let json = serde_json::to_string(&book);
 
     if let Err(e) = json {
-        eprintln!("Could not read save file '{}'.", filename);
+        eprintln!("Could not serialize the TimeBook to json.");
         eprintln!("{}", e);
         std::process::exit(1);
     }
 
     let json = json.unwrap();
-
-    let book = serde_json::from_str(&json);
-
-    if let Err(e) = book {
-        eprintln!("Could parse json from file '{}'.", filename);
+    
+    if let Err(e) = fs::write(&filename, json) {
+        eprintln!("Could not write save file '{}'.", filename);
         eprintln!("{}", e);
         std::process::exit(1);
     }
+}
 
-    let book: TimeBook = book.unwrap();
+fn handle_commands(_book: &mut TimeBook) -> Result<()> {
+    Ok(())
 }
 
